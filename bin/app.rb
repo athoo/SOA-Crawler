c2cfa6943871230b1d31f24d952c87f6e8de5c70
transedward <transedward@gmail.com>
2014-10-23 20:03:15 +0800

    modify some bugs

--- bin/app.rb
+++ bin/app.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 
-#require '../lib/crawler'
-require '/Users/mac/GitHub/SOA-Crawler/lib/crawler.rb'
-puts MovieInfo::movies
+require '../lib/crawler'
+
+puts MovieInfo::movie_review
