7d5a83fc53db9da4474114700f0e9eebb2a7d355
athoo <pingjing.yang@gmail.com>
2014-10-25 01:56:38 +0800

    adding new functions

--- bin/app.rb
+++ bin/app.rb
@@ -1,4 +1,11 @@
 #!/usr/bin/env ruby
 require File.expand_path('../../lib/crawler', __FILE__)
 
-puts MovieInfo.movie_review(ARGV[0] || '')
+# puts MovieInfo.movie_review(ARGV[0] || '')
+
+# puts MovieInfo.dvd_rank
+# puts MovieInfo.us_weekend
+# puts MovieInfo.taipei_weekend
+# puts MovieInfo.movie_review(code)
+
+puts MovieInfo.movies("LATEST") # 'first_round','second_round
--- lib/crawler.rb
+++ lib/crawler.rb
@@ -5,8 +5,7 @@ require 'iconv'
 require 'webrick/httputils'
 
 class MovieInfo
-
-  ATMOVIES_URL = 'http://www.atmovies.com.tw/movie/movie_new.html'
+  LATEST = 'http://www.atmovies.com.tw/movie/movie_new.html'
   MOVIE_BASE_URL = 'http://www.atmovies.com.tw/movie/'
   WHOLE_MOVIEWS_TITLES = "//div[@class = 'title']/a"
   WHOLE_MOVIEWS_STORIES = "//div[@class = 'story']"
@@ -17,21 +16,70 @@ class MovieInfo
   REFLECTION_CLASS = "//div[@class = 'act01']"
   REFLECTION_SATITLE = '&satitle='
   REFLECTION_SAID = '&said='
+  FIRST_ROUND = 'http://www.atmovies.com.tw/movie/movie_now-1.html'
+  SECOND_ROUND = 'http://www.atmovies.com.tw/movie/movie_now2-1.html'
+  ATMOVIES_MAIN_URL = 'http://www.atmovies.com.tw/home/movie_homepage.html'
 
-  def self.movie_review(code = '')
-    code.empty? ? to_yaml(movies) : to_yaml(movie(code))
+  # add three rank parser
+  def self.us_weekend
+    document = open_html(ATMOVIES_MAIN_URL)
+    get_table(document, '1')
   end
 
-  def self.movies
-    document = open_html(ATMOVIES_URL)
+  def self.taipei_weekend
+    document = open_html(ATMOVIES_MAIN_URL)
+    get_table(document, '2')
+  end
+
+  def self.dvd_rank
+    document = open_html(ATMOVIES_MAIN_URL)
+    get_table(document, '3')
+  end
+
+  # parse the ranktable info
+  def self.get_table(doc, rankid)
+    table = doc.xpath("//*[@id = 'ranklist']/div[" + rankid + ']').text
+    table = table.gsub(' : ', ':').gsub(' ', '').split
+    table = table.each { |item| item.gsub(/[\t\r\n]/, '') }
+    table.pop
+    to_yaml(rankmix(table))
+  end
+
+  # mix the rank info
+  def self.rankmix(t)
+    t.each_with_index.map do |_, index|
+      {
+        index + 1 => t[index].to_s
+      }
+    end
+  end
+
+  # switch to different url accordingly
+  def self.movies(category)
+    case category.upcase
+    when 'LATEST'
+      url = LATEST
+    when 'FIRST_ROUND'
+      url = FIRST_ROUND
+    when 'SECOND_ROUND'
+      url = SECOND_ROUND
+    end
+    movies_parser(url)
+  end
+
+  # parse the movies acoordingly
+  def self.movies_parser(url)
+    document = open_html(url)
     titles = get_titles(document)
     stories = get_stories(document)
     dates = get_dates(document)
     codes = get_codes(document)
-    mix(titles, codes, stories, dates)
+    result = mix(titles, codes, stories, dates)
+    to_yaml(result)
   end
-
-  def self.movie(code)
+  
+  # see the specific movie info
+  def self.movie_review(code)
     document = open_html(MOVIE_BASE_URL + code + '/')
     info_said = REFLECTION_SAID + code
     name = get_one_movie_name(document)
@@ -39,7 +87,8 @@ class MovieInfo
     info_uri = REFLECTION_BASE + info_said + info_satitle + REFLECTION_FS
     informations = open_html(info_uri)
     reflection = get_reflection(informations)
-    { name: name, like: reflection[0], unlike: reflection[1] }
+    out = { name: name, like: reflection[0], unlike: reflection[1] }
+    to_yaml(out)
   end
 
   def self.encode_zh(text)
@@ -69,7 +118,7 @@ class MovieInfo
   # get the storyline of movie
   def self.get_stories(doc)
     storylines = doc.xpath(WHOLE_MOVIEWS_STORIES)
-    storylines.map { |story| story.text }
+    storylines.map(&:text) # { |story| story.text }
   end
 
   # get the release dates
